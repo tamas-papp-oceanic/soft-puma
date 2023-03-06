44b135e7942b3781f8ec582456b5284a32d1deea
Tamas Papp <tamas@osukl.com>
2023-03-06 17:01:22 +0000

    change: Started scrollWidth experiment

--- src/routes/devices/Content.svelte
+++ src/routes/devices/Content.svelte
@@ -9,6 +9,7 @@
 
   export let params;
 
+  let width;
   let height;
   let key = null;
   let first = true;
@@ -46,38 +47,40 @@
     pop();
   };
 
-  // Data getters, setters
-  $: if (first && (key != null) && ($queue.length > 0)) {
-    let dat = $queue.at(0);
-    if (title != null) {
-      title += ' - ' + dat.title;
-    }
-    if ((typeof dat.fields !== 'undefined') && Array.isArray(dat.fields) && (dat.fields.length > 0)) {
-      let tmp = new Array();
-      tmp.push({
-        key: "cnt",
-        value: 'Count',
-        sort: false,
-      });
-      for (let i in dat.fields) {
-        let fld = dat.fields[i];
-        if (fld.reserved == null) {
-          tmp.push({
-            key: 'fld' + fld.field,
-            value: fld.title,
-            sort: false,
-          });
-        }
+  function getHeaders() {
+    if (first && (key != null) && ($queue.length > 0)) {
+      let dat = $queue.at(0);
+      if (title != null) {
+        title += ' - ' + dat.title;
       }
-      tmp.push({
-        key: "overflow",
-        empty: true,
-      });
-      headers = JSON.parse(JSON.stringify(tmp));
-      first = false;
+      if ((typeof dat.fields !== 'undefined') && Array.isArray(dat.fields) && (dat.fields.length > 0)) {
+        let tmp = new Array();
+        tmp.push({
+          key: "overflow",
+          empty: true,
+        });
+        tmp.push({
+          key: "cnt",
+          value: 'Count',
+          sort: false,
+        });
+        for (let i in dat.fields) {
+          let fld = dat.fields[i];
+          if (fld.reserved == null) {
+            tmp.push({
+              key: 'fld' + fld.field,
+              value: fld.title,
+              sort: false,
+            });
+          }
+        }
+        headers = JSON.parse(JSON.stringify(tmp));
+        first = false;
+      }
     }
-  }
-  $: {
+  };
+
+  function getCurrent() {
     if ($queue.length > 0) {
       let tmp = new Array();
       let lst = JSON.parse(JSON.stringify($queue[$queue.length - 1]));
@@ -93,65 +96,86 @@
       tmp.push(obj);
       curr = JSON.parse(JSON.stringify(tmp));
     }
-  }
-  $: pagination.pageSize = Math.round(((height * 0.9) / getComputedStyle(document.documentElement).fontSize.replace('px', '')) / 3) - 4;
-  $: if (key != null) {
-    let tmp = new Array();
-    for (let i in $queue) {
-      let dat = $queue[i];
-      if (((dat.cnt - 1) % (pagination.pageSize - 1)) == 0) {
-        let lst = JSON.parse(JSON.stringify($queue[$queue.length - 1]));
-        lst.id = 'first';
-        let end = {
-          id: lst.id,
-          cnt: lst.cnt,
-        };
-        for (let i in lst.fields) {
-          let fld = lst.fields[i];
-          end['fld' + fld.field] = (fld.state == 'V') ? fld.value : '-';
+  };
+
+  function getRows() {
+    if (key != null) {
+      let tmp = new Array();
+      for (let i in $queue) {
+        let dat = $queue[i];
+        if (((dat.cnt - 1) % (pagination.pageSize - 1)) == 0) {
+          let lst = JSON.parse(JSON.stringify($queue[$queue.length - 1]));
+          lst.id = 'first';
+          let end = {
+            overflow: '',
+            id: lst.id,
+            cnt: lst.cnt,
+          };
+          for (let i in lst.fields) {
+            let fld = lst.fields[i];
+            end['fld' + fld.field] = (fld.state == 'V') ? fld.value : '-';
+          }
+          tmp.push(end);
         }
-        end['overflow'] = '';
-        tmp.push(end);
+        let obj = {
+          id: dat.id,
+          cnt: dat.cnt,
+        };
+        for (let i in dat.fields) {
+          let fld = dat.fields[i];
+          obj['fld' + fld.field] = (fld.state == 'V') ? fld.value : '-';
+        }
+        tmp.push(obj);
       }
-      let obj = {
-        id: dat.id,
-        cnt: dat.cnt,
-      };
-      for (let i in dat.fields) {
-        let fld = dat.fields[i];
-        obj['fld' + fld.field] = (fld.state == 'V') ? fld.value : '-';
-      }
-      tmp.push(obj);
+      rows = JSON.parse(JSON.stringify(tmp));
+      pagination.totalItems = rows.length;
     }
-    rows = JSON.parse(JSON.stringify(tmp));
-    pagination.totalItems = rows.length;
-  }
+  };
+
+  function getWidth(sel) {
+    let box = document.querySelector(sel);
+    if (box != null) {
+
+      console.log(box, box.scrollWidth, box.offsetWidth)
+
+      return box.scrollWidth;
+    }
+    return 0;
+  };
+
+  // Data getters, setters
+  $: pagination.pageSize = Math.round(((height * 0.9) / getComputedStyle(document.documentElement).fontSize.replace('px', '')) / 3) - (getWidth('.tlbr') < getWidth('.last table') ? 5 : 4);
+  $: $queue, getHeaders(); 
+  $: $queue, getCurrent(); 
+  $: $queue, getRows(); 
 </script>
 
-<svelte:window bind:innerHeight={height} />
+<svelte:window bind:innerHeight={height} bind:innerWidth={width} />
 <Grid>
   <Row>
     <Column>
       {#if headers.length > 0}
-        <div class="cont">
-          <DataTable {headers} {rows} pageSize={pagination.pageSize} page={pagination.page} class="last">
-            <Toolbar>
-              <Tile>{title}</Tile>
-              <ToolbarContent>
-                <Button icon={Restart} on:click={(e) => rest(e)}>Restart</Button>
-                <Button icon={SkipBack} on:click={(e) => back(e)}>Back</Button>
-              </ToolbarContent>
-            </Toolbar>
-            <span slot="cell" let:cell let:row>
-              {#if (cell.key === 'overflow') && (row.id !== 'first')}
-                <OverflowMenu flipped>
-                  <OverflowMenuItem text="Details" disabled on:click={() => push('/details/'+ row.key)} />
-                </OverflowMenu>
-              {:else}
-                {cell.value}
-              {/if}
-            </span>
-          </DataTable>
+        <Toolbar class="tlbr">
+          <Tile>{title}</Tile>
+          <ToolbarContent>
+            <Button icon={Restart} on:click={(e) => rest(e)}>Restart</Button>
+            <Button icon={SkipBack} on:click={(e) => back(e)}>Back</Button>
+          </ToolbarContent>
+        </Toolbar>
+        <div class="ctnr flip">
+          <div class="cont">
+            <DataTable {headers} {rows} pageSize={pagination.pageSize} page={pagination.page} class="last">
+              <span slot="cell" let:cell let:row>
+                {#if (cell.key === 'overflow') && (row.id !== 'first')}
+                  <OverflowMenu>
+                    <OverflowMenuItem text="Details" disabled on:click={() => push('/details/'+ row.key)} />
+                  </OverflowMenu>
+                {:else}
+                  {cell.value}
+                {/if}
+              </span>
+            </DataTable>
+          </div>
         </div>
       {/if}
       {#if pagination.totalItems > pagination.pageSize}
@@ -167,9 +191,15 @@
 </Grid>
 
 <style global>
-  .cont {
+  .ctnr {
     overflow-x: auto;
     overflow-y: hidden;
+  }
+  .cont {
+    width: 100%;
+  }
+  .flip, .flip .cont{
+    transform: rotateX(180deg);
   }
   .last table tbody tr:first-child td {
     background: #c6c6c6;
