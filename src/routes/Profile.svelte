e6a8f41e7a3272faf3755e7330512ba33219efad
Tamas Papp <tamas@osukl.com>
2023-12-05 16:46:11 +0000

    change: Improved user handling

--- src/routes/Profile.svelte
+++ src/routes/Profile.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from 'svelte'
   import { push } from 'svelte-spa-router'
-  import { Grid, Row, Column, ToastNotification, TextInput, PasswordInput, Button,
+  import { Form, Grid, Row, Column, ToastNotification, TextInput, PasswordInput, Button,
     ComposedModal, ModalHeader, ModalBody, ModalFooter } from "carbon-components-svelte";
   import { afetch } from '../auth/auth.js'
   import { authURL, userData } from '../stores/user.js'
@@ -24,19 +24,13 @@
   };
 
   async function _submit() {
-    errtext = ''
     error = false;
-    if ((password == null) || (password.length == 0)) {
-      errtext = 'Password cannot be empty.'
-      error = true;
-      return;
-    }
     if ((newpass != null) && (newpass != rptpass)) {
       errtext = 'New password and Repeat password fields are different.'
       error = true;
       return;
     }
-    if (email != null) {
+    if ((typeof email === 'string') && (email !== '')) {
       let val = _email(email);
       if (typeof val === 'string') {
         errtext = val;
@@ -91,67 +85,69 @@
   }
 </script>
 
-<Grid>
-  <Row>
-    <Column sm={0} md={2} lg={4} />
-    <Column sm={4} md={4} lg={8}>
-      <Row>
-        <Column>
-          <h4>User profile information</h4>
-          <p class="descr">(to change current password fill up new password and repeat fields)</p>
-          <hr>
-        </Column>
-      </Row>
-      <Row>
-        <Column>
-          <TextInput readonly bind:value={username} labelText="User name" placeholder="Enter user name..." required />
-        </Column>
-      </Row>
-      <Row padding>
-        <Column>
-          <PasswordInput id="password" bind:value={password} type="password" labelText="Current password" placeholder="Enter current password..." required />
-        </Column>
-      </Row>
-      <Row>
-        <Column>
-          <PasswordInput bind:value={newpass} type="password" labelText="New password" placeholder="Enter new password..." required />
-        </Column>
-      </Row>
-      <Row padding>
-        <Column>
-          <PasswordInput warn={(newpass !== null) && (rptpass != newpass)} bind:value={rptpass} type="password" labelText="Repeat password" warnText="Differs from new password!" placeholder="Repeat new password..." />
-        </Column>
-      </Row>
-      <Row>
-        <Column>
-          <TextInput bind:value={email} labelText="Email" placeholder="Enter email address..." />
-        </Column>
-      </Row>
-      <Row padding>
-        <Column>
-          <Button on:click={(e) => _submit(e)} type="submit">Submit</Button>
-          <Button kind="secondary" on:click={(e) => _quit(e)}>Cancel</Button>
-        </Column>
-        <Column style="text-align: end;">
-          <Button kind="tertiary" on:click={(e) => _delete(e)}>Delete</Button>
-        </Column>
-      </Row>
-    </Column>
-    <Column sm={0} md={2} lg={4} />
-  </Row>
-  {#if error}
+<Form>
+  <Grid>
     <Row>
-      <ToastNotification
-        lowContrast=false
-        fullWidth
-        kind="error"
-        title="Error"
-        subtitle={errtext}
-        hideCloseButton
-      />
+      <Column sm={0} md={2} lg={4} />
+      <Column sm={4} md={4} lg={8}>
+        <Row>
+          <Column>
+            <h4>User profile information</h4>
+            <p class="descr">(to change current password fill up new password and repeat fields)</p>
+            <hr>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <TextInput readonly bind:value={username} labelText="User name" placeholder="Enter user name..." required />
+          </Column>
+        </Row>
+        <Row padding>
+          <Column>
+            <PasswordInput id="password" bind:value={password} type="password" labelText="Current password" placeholder="Enter current password..." required />
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <PasswordInput bind:value={newpass} type="password" labelText="New password" placeholder="Enter new password..." />
+          </Column>
+        </Row>
+        <Row padding>
+          <Column>
+            <PasswordInput warn={(newpass !== null) && (rptpass != newpass)} bind:value={rptpass} type="password" labelText="Repeat password" warnText="Differs from new password!" placeholder="Repeat new password..." />
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <TextInput bind:value={email} labelText="Email" placeholder="Enter email address..." />
+          </Column>
+        </Row>
+        <Row padding>
+          <Column>
+            <Button on:click={(e) => _submit(e)} type="submit">Submit</Button>
+            <Button kind="secondary" on:click={(e) => _quit(e)}>Cancel</Button>
+          </Column>
+          <Column style="text-align: end;">
+            <Button kind="tertiary" on:click={(e) => _delete(e)}>Delete</Button>
+          </Column>
+        </Row>
+      </Column>
+      <Column sm={0} md={2} lg={4} />
     </Row>
-  {/if}
-</Grid>
+    {#if error}
+      <Row>
+        <ToastNotification
+          lowContrast=false
+          fullWidth
+          kind="error"
+          title="Error"
+          subtitle={errtext}
+          hideCloseButton
+        />
+      </Row>
+    {/if}
+  </Grid>
+</Form>
 <ComposedModal open={remove} on:submit={(e) => _remove(e)} on:close={(e) => _cancel(e)} size="xs">
   <ModalHeader title="Are you sure you want to delete your account?" />
   <ModalBody>
